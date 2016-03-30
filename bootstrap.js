7cfeab79762d15f156f480d989834918164ae4a3
JustOff <Off.Just.Off@gmail.com>
2016-03-30 17:59:03 +0300

    modhresp: +toolbar button

--- bootstrap.js
+++ bootstrap.js
@@ -3,7 +3,9 @@
 var {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
 Cu.import("resource://gre/modules/Services.jsm");
 
-var enabled, fArray, branch = "extensions.modhresponse.";
+var enabled, fArray, desktop, gWindowListener, branch = "extensions.modhresponse.";
+var styleSheetService = Cc["@mozilla.org/content/style-sheet-service;1"].getService(Ci.nsIStyleSheetService);
+var styleSheetURI = Services.io.newURI("chrome://modhresponse/skin/modhresponse.css", null, null);
 
 var httpObserver = {
 	observe: function(subject, topic, data) {
@@ -169,9 +171,188 @@ var prefObserver = {
 	}
 }
 
+function $(node, childId) {
+	if (node.getElementById) {
+		return node.getElementById(childId);
+	} else {
+		return node.querySelector("#" + childId);
+	}
+}
+
+function bImg(b, img) {
+	b.style.listStyleImage = 'url("chrome://modhresponse/skin/' + img + '.png")';
+}
+
+var button = {
+	meta : {
+		id : "modhresponse-button",
+		label : "Modify HTTP Response",
+		tooltiptext : "Modify HTTP Response",
+		class : "toolbarbutton-1 chromeclass-toolbar-additional"
+	},
+	install : function(w) {
+		var doc = w.document;
+		var b = doc.createElement("toolbarbutton");
+		for (var a in this.meta) {
+			b.setAttribute(a, this.meta[a]);
+		}
+
+		var toolbox = $(doc, "navigator-toolbox");
+		toolbox.palette.appendChild(b);
+
+		var {toolbarId, nextItemId} = this.getPrefs(),
+			toolbar = toolbarId && $(doc, toolbarId),
+			nextItem = toolbar && $(doc, nextItemId);
+		if (toolbar) {
+			if (nextItem && nextItem.parentNode && nextItem.parentNode.id.replace("-customization-target", "") == toolbarId) {
+				toolbar.insertItem(this.meta.id, nextItem);
+			} else {
+				var ids = (toolbar.getAttribute("currentset") || "").split(",");
+				nextItem = null;
+				for (var i = ids.indexOf(this.meta.id) + 1; i > 0 && i < ids.length; i++) {
+					nextItem = $(doc, ids[i])
+					if (nextItem) {
+						break;
+					}
+				}
+				toolbar.insertItem(this.meta.id, nextItem);
+			}
+			try { w.setToolbarVisibility(toolbar, true); } catch(e) {}
+		}
+		return b;
+	},
+	onCustomize : function(e) {
+		var ucs = Services.prefs.getCharPref("browser.uiCustomization.state");
+		if ((/\"nav\-bar\"\:\[.*?\"modhresponse\-button\".*?\]/).test(ucs)) {
+			Services.prefs.getBranch(branch).setCharPref("toolbarId", "nav-bar");
+		} else {
+			button.setPrefs(null, null);
+		}
+	},
+	afterCustomize : function(e) {
+		var toolbox = e.target,
+			b = $(toolbox.parentNode, button.meta.id),
+			toolbarId, nextItemId;
+		if (b) {
+			var parent = b.parentNode,
+				nextItem = b.nextSibling;
+			if (parent && (parent.localName == "toolbar" || parent.classList.contains("customization-target"))) {
+				toolbarId = parent.id;
+				nextItemId = nextItem && nextItem.id;
+			}
+		}
+		button.setPrefs(toolbarId, nextItemId);
+	},
+	getPrefs : function() {
+		var p = Services.prefs.getBranch(branch);
+		return {
+			toolbarId : p.getCharPref("toolbarId"),
+			nextItemId : p.getCharPref("nextItemId")
+		};
+	},
+	setPrefs : function(toolbarId, nextItemId) {
+		var p = Services.prefs.getBranch(branch);
+		p.setCharPref("toolbarId", toolbarId == "nav-bar-customization-target" ? "nav-bar" : toolbarId || "");
+		p.setCharPref("nextItemId", nextItemId || "");
+	}
+};
+
+var buttonInject = function(w) {
+	var b = button.install(w);
+
+	var windowPrefsWatcher = {
+		observe: function(subject, topic, data) {
+			if (topic != "nsPref:changed") return;
+			switch (data) {
+				case "enabled":
+					if (Services.prefs.getBranch(branch).getBoolPref("enabled")) {
+						bImg(b, "icon");
+					} else {
+						bImg(b, "icoff");
+					}
+				break;
+			}
+		},
+		register: function() {
+			var prefsService = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService);
+			this.prefBranch = prefsService.getBranch(branch);
+			this.prefBranch.addObserver("", this, false);
+		},
+		unregister: function() {
+			this.prefBranch.removeObserver("", this);
+		}
+	}
+
+	return {
+		init : function() {
+			windowPrefsWatcher.register();
+			w.addEventListener("customizationchange", button.onCustomize, false);
+			w.addEventListener("aftercustomization", button.afterCustomize, false);
+			b.addEventListener("command", this.run, false);
+			bImg(b, enabled ? "icon" : "icoff");
+		},
+		done : function() {
+			windowPrefsWatcher.unregister();
+			w.removeEventListener("customizationchange", button.onCustomize, false);
+			w.removeEventListener("aftercustomization", button.afterCustomize, false);
+			b.removeEventListener("command", this.run, false);
+			b.parentNode.removeChild(b);
+			b = null;
+		},
+		run : function(e) {
+			if (e.ctrlKey || e.metaKey) {
+				Services.wm.getMostRecentWindow("navigator:browser").BrowserOpenAddonsMgr("addons://detail/modhresponse@Off.JustOff/preferences");
+			} else {
+				Services.prefs.getBranch(branch).setBoolPref("enabled", !enabled);
+			}
+		}
+	};
+};
+
+function browserWindowObserver(handlers) {
+	this.handlers = handlers;
+}
+
+browserWindowObserver.prototype = {
+	observe: function(aSubject, aTopic, aData) {
+		if (aTopic == "domwindowopened") {
+			aSubject.QueryInterface(Ci.nsIDOMWindow).addEventListener("load", this, false);
+		} else if (aTopic == "domwindowclosed") {
+			if (aSubject.document.documentElement.getAttribute("windowtype") == "navigator:browser") {
+				this.handlers.onShutdown(aSubject);
+			}
+		}
+	},
+	handleEvent: function(aEvent) {
+		let aWindow = aEvent.currentTarget;
+		aWindow.removeEventListener(aEvent.type, this, false);
+
+		if (aWindow.document.documentElement.getAttribute("windowtype") == "navigator:browser") {
+			this.handlers.onStartup(aWindow);
+		}
+	}
+};
+
+function browserWindowStartup(aWindow) {
+	aWindow.modhresponse = buttonInject(aWindow);
+	aWindow.modhresponse.init()
+}
+
+function browserWindowShutdown(aWindow) {
+	aWindow.modhresponse.done();
+	delete aWindow.modhresponse;
+}
+
 function startup(data, reason) {
 	Cu.import("chrome://modhresponse/content/prefloader.js");
 	PrefLoader.loadDefaultPrefs(data.installPath, "modhresponse.js");
+	
+	var appInfo = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULAppInfo);
+	desktop = (appInfo.ID != "{aa3c5121-dab2-40e2-81ca-7ea25febc110}");
+
+	if (desktop && !styleSheetService.sheetRegistered(styleSheetURI, styleSheetService.USER_SHEET)) {
+		styleSheetService.loadAndRegisterSheet(styleSheetURI, styleSheetService.USER_SHEET);
+	}
 
 	enabled = Services.prefs.getBranch(branch).getBoolPref("enabled");
 	if (enabled) {
@@ -183,13 +364,45 @@ function startup(data, reason) {
 		}
 	}
 	prefObserver.register();
+
+	if (desktop) {
+		var ww = Cc["@mozilla.org/embedcomp/window-watcher;1"].getService(Ci.nsIWindowWatcher);
+		gWindowListener = new browserWindowObserver({
+			onStartup: browserWindowStartup,
+			onShutdown: browserWindowShutdown
+		});
+		ww.registerNotification(gWindowListener);
+	
+		var wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+		var winenu = wm.getEnumerator("navigator:browser");
+		while (winenu.hasMoreElements()) {
+			browserWindowStartup(winenu.getNext());
+		}
+	}
 }
 
 function shutdown(data, reason) {
 	if (reason == APP_SHUTDOWN) return;
 
+	if (desktop) {
+		var ww = Cc["@mozilla.org/embedcomp/window-watcher;1"].getService(Ci.nsIWindowWatcher);
+		ww.unregisterNotification(gWindowListener);
+		gWindowListener = null;
+
+		var wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+		var winenu = wm.getEnumerator("navigator:browser");
+		while (winenu.hasMoreElements()) {
+			browserWindowShutdown(winenu.getNext());
+		}
+	}
+
 	prefObserver.unregister();
 	httpObserver.unregister();
+
+	if (desktop && styleSheetService.sheetRegistered(styleSheetURI, styleSheetService.USER_SHEET)) {
+		styleSheetService.unregisterSheet(styleSheetURI, styleSheetService.USER_SHEET);
+	}
+
 	Cu.unload("chrome://modhresponse/content/prefloader.js");
 }
 
--- defaults/preferences/modhresponse.js
+++ defaults/preferences/modhresponse.js
@@ -1,2 +1,4 @@
 pref("extensions.modhresponse.enabled", false);
 pref("extensions.modhresponse.filter", "");
+pref("extensions.modhresponse.toolbarId", "nav-bar");
+pref("extensions.modhresponse.nextItemId", "");
