2703f2b902551c3a9730776eb9c9f81218f3e19e
JustOff <Off.Just.Off@gmail.com>
2021-02-22 17:38:12 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,9 @@
+@echo off
+set VER=1.3.6
+
+sed -i -E "s/version>.+?</version>%VER%</" install.rdf
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/modhresponse-.+?\.xpi/download\/%VER%\/modhresponse-%VER%\.xpi/" update.xml
+
+set XPI=modhresponse-%VER%.xpi
+if exist %XPI% del %XPI%
+zip -r9q %XPI% * -x .git/* .gitignore update.xml LICENSE README.md *.cmd *.xpi *.exe
